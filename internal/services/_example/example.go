fbe02719ebf728fadaaea37369517cdd51bcfec9
Raphael Jacob <r.jacob2002@gmail.com>
2020-04-07 16:24:40 +0200

    Update example service

--- internal/services/_example/example.go
+++ internal/services/_example/example.go
@@ -9,12 +9,14 @@ type Example interface {
 	MethodTwo(_ string, _ int) bool
 }
 
-func (e *example) Init(map[string]service.Service, func(string), func(error)) error { return nil }
-func (e *example) Stop() error                                                      { return nil }
-func (e *example) Name() string                                                     { return "example" }
-func (e *example) Dependencies() ([]string, []string)                               { return []string{}, []string{} }
-func (e *example) SetAdditionalDependencies(map[string]service.Service) error       { return nil }
-func (e *example) UnsetAdditionalDependencies(s chan interface{})                   { s <- struct{}{} }
+func (e *example) Init(map[string]service.Service, service.LoggerFunc, service.ErrorFunc) error {
+	return nil
+}
+func (e *example) Stop() error                                                { return nil }
+func (e *example) Name() string                                               { return "example" }
+func (e *example) Dependencies() ([]string, []string)                         { return []string{}, []string{} }
+func (e *example) SetAdditionalDependencies(map[string]service.Service) error { return nil }
+func (e *example) UnsetAdditionalDependencies(s chan interface{})             { s <- struct{}{} }
 
 func (e *example) MethodOne()                     {}
 func (e *example) MethodTwo(_ string, _ int) bool { return true }
