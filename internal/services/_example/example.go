85828c117704b0e5b0500b812f82134ef348dfd7
Raphael Jacob <r.jacob2002@gmail.com>
2020-05-02 19:24:16 +0200

    Improve example service

--- internal/services/_example/example.go
+++ internal/services/_example/example.go
@@ -1,24 +1,58 @@
 package example
 
-import "github.com/ftCommunity/roboheart/internal/service"
+import (
+	"time"
 
-type example struct{}
+	"github.com/ftCommunity/roboheart/internal/service"
+	"github.com/ftCommunity/roboheart/package/threadmanager"
+)
+
+type example struct {
+	logger service.LoggerFunc
+	error  service.ErrorFunc
+	tm     *threadmanager.ThreadManager
+}
 
 type Example interface {
 	MethodOne()
 	MethodTwo(_ string, _ int) bool
 }
 
-func (e *example) Init(map[string]service.Service, service.LoggerFunc, service.ErrorFunc) error {
+func (ex *example) Init(services map[string]service.Service, logger service.LoggerFunc, e service.ErrorFunc) error {
+	ex.logger = logger
+	ex.error = e
+	ex.tm = threadmanager.NewThreadManager(ex.logger, ex.error)
+	ex.tm.Load("cleanup", ex.exampleThread)
+	ex.tm.Start("cleanup")
 	return nil
 }
-func (e *example) Stop() error                                                { return nil }
-func (e *example) Name() string                                               { return "example" }
-func (e *example) Dependencies() ([]string, []string)                         { return []string{}, []string{} }
-func (e *example) SetAdditionalDependencies(map[string]service.Service) error { return nil }
-func (e *example) UnsetAdditionalDependencies(s chan interface{})             { s <- struct{}{} }
 
-func (e *example) MethodOne()                     {}
-func (e *example) MethodTwo(_ string, _ int) bool { return true }
+func (ex *example) Stop() error                                                { return nil }
+func (ex *example) Name() string                                               { return "example" }
+func (ex *example) Dependencies() ([]string, []string)                         { return []string{}, []string{} }
+func (ex *example) SetAdditionalDependencies(map[string]service.Service) error { return nil }
+func (ex *example) UnsetAdditionalDependencies(s chan interface{})             { s <- struct{}{} }
+
+func (ex *example) exampleThread(logger service.LoggerFunc, e service.ErrorFunc, stop, stopped chan interface{}) {
+	// for a normal "do every x seconds"-thread you should not need to change too much
+	//if your task should run once beforehand you might just put it right before the for loop
+	for {
+		select {
+		case <-stop:
+			{
+				stopped <- struct{}{}
+				return
+			}
+		//change time here
+		case <-time.After(5 * time.Second):
+			{
+				//task
+			}
+		}
+	}
+}
+
+func (ex *example) MethodOne()                     {}
+func (ex *example) MethodTwo(_ string, _ int) bool { return true }
 
 var Service = new(example)
