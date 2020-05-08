340af6ee4e3a98a6362afd76fba3c1849cb8a134
Raphael Jacob <r.jacob2002@gmail.com>
2020-05-08 22:37:32 +0200

    doc

--- cmd/roboheart/main.go
+++ cmd/roboheart/main.go
@@ -11,19 +11,25 @@ import (
 
 func main() {
 	log.Println("Starting roboheart")
+	//create ServiceManager
 	sm, err := servicemanager.NewServiceManager()
 	if err != nil {
 		log.Fatal(err)
 	}
+	//inititialize ServiceManager
 	err = sm.Init()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("Start-up completed")
+	//setup ctrl-c interrupt
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	//wait for ctrl-c
 	<-c
+	//initiate stop procedure
 	log.Println("Stopping roboheart")
 	sm.Stop()
-	log.Println("End")
+	log.Println("Heart rate zero")
+	log.Println("Dead")
 }
