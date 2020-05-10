e9ce34494d94750ea40bed130ab58f39f076e3a2
Raphael Jacob <r.jacob2002@gmail.com>
2020-05-10 14:17:12 +0200

    Service fwver: Add web hook

--- internal/services/core/fwver/fwver.go
+++ internal/services/core/fwver/fwver.go
@@ -1,16 +1,24 @@
 package fwver
 
 import (
+	"errors"
 	"io/ioutil"
+	"net/http"
 	"strings"
 
 	"github.com/blang/semver"
 	"github.com/ftCommunity/roboheart/internal/service"
+	"github.com/ftCommunity/roboheart/internal/services/core/web"
+	"github.com/ftCommunity/roboheart/package/api"
+	"github.com/ftCommunity/roboheart/package/servicehelpers"
+	"github.com/gorilla/mux"
 )
 
 type fwver struct {
 	rawver string
 	semver semver.Version
+	web    web.Web
+	mux    *mux.Router
 }
 
 type FWVer interface {
@@ -31,11 +39,30 @@ func (f *fwver) Init(map[string]service.Service, service.LoggerFunc, service.Err
 	return nil
 }
 
-func (f *fwver) Stop() error                                                { return nil }
-func (f *fwver) Name() string                                               { return "fwver" }
-func (f *fwver) Dependencies() ([]string, []string)                         { return []string{}, []string{} }
-func (f *fwver) SetAdditionalDependencies(map[string]service.Service) error { return nil }
-func (f *fwver) UnsetAdditionalDependencies()                               {}
+func (f *fwver) Stop() error                        { return nil }
+func (f *fwver) Name() string                       { return "fwver" }
+func (f *fwver) Dependencies() ([]string, []string) { return []string{}, []string{"web"} }
+func (f *fwver) SetAdditionalDependencies(services map[string]service.Service) error {
+	if err := servicehelpers.CheckAdditionalDependencies(f, services); err != nil {
+		return err
+	}
+	var ok bool
+	f.web, ok = services["web"].(web.Web)
+	if !ok {
+		return errors.New("Type assertion error")
+	}
+	f.configureWeb()
+	return nil
+}
+
+func (f *fwver) UnsetAdditionalDependencies() {}
+
+func (f *fwver) configureWeb() {
+	f.mux = f.web.RegisterServiceAPI(f)
+	f.mux.HandleFunc("/version", func(w http.ResponseWriter, _ *http.Request) {
+		api.ResponseWriter(w, 200, f.semver.String())
+	})
+}
 
 func (f *fwver) Get() semver.Version {
 	return f.semver
