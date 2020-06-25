f1a574b3fd0b2ff10398b84fa517dc47d20ac3ac
Raphael Jacob <r.jacob2002@gmail.com>
2020-06-25 16:05:45 +0200

    Add checkdeps tool

---
+++ cmd/checkdeps/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/ftCommunity/roboheart/internal/servicemanager"
+	"log"
+)
+
+func main() {
+	log.Println("Checking dependencies")
+	if _, err := servicemanager.NewServiceManager(); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Dependencies ok!")
+}
