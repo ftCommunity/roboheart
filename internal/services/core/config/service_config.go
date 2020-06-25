78a983370488be0118c55c118fca53f6038aacb6
Raphael Jacob <r.jacob2002@gmail.com>
2020-06-25 16:07:19 +0200

    Service config: make ServiceConfig public

--- internal/services/core/config/service_config.go
+++ internal/services/core/config/service_config.go
@@ -1,38 +1,38 @@
 package config
 
-type serviceConfig struct {
+type ServiceConfig struct {
 	c        *config
 	confname string
 }
 
-func (sc *serviceConfig) GetSections(secType string) ([]string, bool) {
+func (sc *ServiceConfig) GetSections(secType string) ([]string, bool) {
 	return sc.c.tree.GetSections(sc.confname, secType)
 }
 
-func (sc *serviceConfig) Get(section, option string) ([]string, bool) {
+func (sc *ServiceConfig) Get(section, option string) ([]string, bool) {
 	return sc.c.tree.Get(sc.confname, section, option)
 }
 
-func (sc *serviceConfig) GetLast(section, option string) (string, bool) {
+func (sc *ServiceConfig) GetLast(section, option string) (string, bool) {
 	return sc.c.tree.GetLast(sc.confname, section, option)
 }
 
-func (sc *serviceConfig) GetBool(section, option string) (bool, bool) {
+func (sc *ServiceConfig) GetBool(section, option string) (bool, bool) {
 	return sc.c.tree.GetBool(sc.confname, section, option)
 }
 
-func (sc *serviceConfig) Set(section, option string, values ...string) bool {
+func (sc *ServiceConfig) Set(section, option string, values ...string) bool {
 	return sc.c.tree.Set(sc.confname, section, option, values...)
 }
 
-func (sc *serviceConfig) Del(section, option string) { sc.c.tree.Del(sc.confname, section, option) }
+func (sc *ServiceConfig) Del(section, option string) { sc.c.tree.Del(sc.confname, section, option) }
 
-func (sc *serviceConfig) AddSection(section, typ string) error {
+func (sc *ServiceConfig) AddSection(section, typ string) error {
 	return sc.c.tree.AddSection(sc.confname, section, typ)
 }
 
-func (sc *serviceConfig) DelSection(section string) { sc.c.tree.DelSection(sc.confname, section) }
+func (sc *ServiceConfig) DelSection(section string) { sc.c.tree.DelSection(sc.confname, section) }
 
-func newServiceConfig(c *config, confname string) *serviceConfig {
-	return &serviceConfig{c, confname}
+func newServiceConfig(c *config, confname string) *ServiceConfig {
+	return &ServiceConfig{c, confname}
 }
